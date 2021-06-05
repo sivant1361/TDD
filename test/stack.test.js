243f18c47c4fc36c34c0f2a368af38f50ca40655
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-06-05 22:15:23 +0530

    test driven development basics

---
+++ test/stack.test.js
@@ -0,0 +1,34 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+}
+
+describe("My Stack", () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it("it is create empty", () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+  it("can push to the top", () => {
+    stack.push("🥑");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("🥑");
+  });
+  it.todo("can pop off");
+});
